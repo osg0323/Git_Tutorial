59317bb1e8543596ef05d4db9829b88c5adb4a10
osg0323 <osg0323@naver.com>
2020-08-19 12:18:19 +0900

    Add UserController [Update]

--- UserController.java
+++ UserController.java
@@ -16,7 +16,14 @@ public class UserController {
 		logger.info("경로 :lnsert");
 
 		return "user/lnsert";
+	}
 
+	//회원수정 페이지로 이동
+	@RequestMapping("/user/update.do")
+	public String update(){
+		logger.info("경로 :update");
+
+		return "user/update";
 	}
 
 }
